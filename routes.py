84ef930c69d3b3b774f4ea3037a897cee70178b1
Samuel Khzym <smryk7@gmail.com>
2021-05-08 12:36:17 -0400

    Added generate_returns(df) function that returns a list of 28 returns columns

--- routes.py
+++ routes.py
@@ -1,1 +1,17 @@
 #routes file
+import ta
+import pandas as pd
+
+def generate_returns(df):
+
+    TIME_WINDOW = 28
+    OPEN_NAME = "Open"
+    CLOSE_NAME = "Close"
+
+    returns_columns = []
+
+    for window in range(TIME_WINDOW):
+        returns = df[CLOSE_NAME] - df[OPEN_NAME].shift(periods=window)
+        returns_columns.append(returns)
+
+    return returns_columns
