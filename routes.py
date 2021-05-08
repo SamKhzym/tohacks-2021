29245b0e3b43fc87520dbb2db446718414d7b99d
Samuel Khzym <smryk7@gmail.com>
2021-05-08 13:50:10 -0400

    Added API calls and TA support for daily timeseries dataframe

--- routes.py
+++ routes.py
@@ -1,17 +1,38 @@
+<<<<<<< Updated upstream
 #routes file
+=======
+from flask import Flask
+from alpha_vantage.timeseries import TimeSeries
+>>>>>>> Stashed changes
 import ta
 import pandas as pd
 
+app = Flask(__name__)
+
+#Generates return columns from 1-28 days
 def generate_returns(df):
 
     TIME_WINDOW = 28
-    OPEN_NAME = "Open"
-    CLOSE_NAME = "Close"
+    OPEN_NAME = "open"
+    CLOSE_NAME = "adjusted_close"
 
     returns_columns = []
 
     for window in range(TIME_WINDOW):
-        returns = df[CLOSE_NAME] - df[OPEN_NAME].shift(periods=window)
+        returns = (df[CLOSE_NAME] - df[OPEN_NAME].shift(periods=window))/df[OPEN_NAME].shift(periods=window)
         returns_columns.append(returns)
 
+<<<<<<< Updated upstream
     return returns_columns
+=======
+    return returns_columns
+
+#Returns daily dataframe of specified stock ticker with all bukosabino/ta indicators
+def get_stock_data(ticker):
+    df = pd.read_csv("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol="+ticker+"&apikey=L6INA3MM73RWUHIO&datatype=csv&outputsize=full")
+    df = df.iloc[::-1]
+
+    df = ta.add_all_ta_features(df, open="open", high="high", low="low", close="adjusted_close", volume="volume")
+
+    return df
+>>>>>>> Stashed changes
