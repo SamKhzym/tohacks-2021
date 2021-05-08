0ceb6ba256a452c8d0bc44929d182dd60a5061d1
Ariel Wolle <36164850+ArielWolle@users.noreply.github.com>
2021-05-08 12:53:12 -0400

    added example

--- routes.py
+++ routes.py
@@ -1,1 +1,20 @@
-#routes file
+# routes file
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+"""
+TECH INDICATORS Example
+
+from alpha_vantage.techindicators import TechIndicators
+import matplotlib.pyplot as plt
+
+ti = TechIndicators(key='YOUR_API_KEY', output_format='pandas')
+data, meta_data = ti.get_bbands(symbol='MSFT', interval='60min', time_period=60)
+data.plot()
+plt.title('BBbands indicator for  MSFT stock (60 min)')
+plt.show()
+
+"""
