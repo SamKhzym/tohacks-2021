6c4ec62a74743005af451480a3584853139f1c23
Ariel Wolle <36164850+ArielWolle@users.noreply.github.com>
2021-05-09 18:47:15 -0400

    fixed tts

--- pages.py
+++ pages.py
@@ -47,7 +47,7 @@ def results():
 def say():
     text = request.args.get("text")
     return redirect(
-        "http://www.arielwolle.com:9191/sas/?text=" + text + " is a good investment"
+        "http://www.arielwolle.com:9191/sas/?text=Hello my guy" + text + " is a good investment"
     )
 
 
